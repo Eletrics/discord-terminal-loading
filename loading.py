ee1c6bc80ce878d1a9c087b69f693cd108dc12b4
Eletrics <83491934+Eletrics@users.noreply.github.com>
2021-09-12 20:22:05 -0400

    Create loading.py

---
+++ loading.py
@@ -0,0 +1,24 @@
+import os, time, colorama
+from time import sleep
+from colorama import Fore
+
+print("Loading")
+time.sleep(1)
+os.system('cls')
+print("Loading.")
+time.sleep(1)
+os.system('cls')
+print("Loading..")
+time.sleep(1)
+os.system('cls')
+print("Loading...")
+time.sleep(1)
+os.system('cls')
+print("Finished loading! opening up (your project name)")
+time.sleep(3)
+os.system('cls')
+print(Fore.RED +"▄▄▌         ▄▄▄· ·▄▄▄▄  ▪   ▐ ▄  ▄▄ • ")
+print(Fore.RED +"██•  ▪     ▐█ ▀█ ██▪ ██ ██ •█▌▐█▐█ ▀ ▪")
+print(Fore.RED +"██▪   ▄█▀▄ ▄█▀▀█ ▐█· ▐█▌▐█·▐█▐▐▌▄█ ▀█▄")
+print(Fore.RED +"▐█▌▐▌▐█▌.▐▌▐█ ▪▐▌██. ██ ▐█▌██▐█▌▐█▄▪▐█")
+print(Fore.RED +".▀▀▀  ▀█▄▀▪ ▀  ▀ ▀▀▀▀▀• ▀▀▀▀▀ █▪·▀▀▀▀")
